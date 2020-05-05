08ae4f359f66d1e2f064568f6210015c39974f18
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-05 18:03:28 -0400

    computing statistics

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        private List<double> grades;
+        private string name;
+        
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,45 +3,42 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            // double x = 31.3;
-            // int y = 100;
-            // var result = x + y;
-            // Console.WriteLine(result);
-            // System.Console.WriteLine();
 
-            var numbers = new [] {12.7, 52.1, 11.5};
-            // numbers[0] = 12.7;
-            // numbers[1] = 52.1;
-            // numbers[2] = 11.5;
+
+            var book = new Book("Joao's Book");
+            book.AddGrade(98.9);
+            book.AddGrade(90.5);
+            book.AddGrade(100.0);
+
+            // var numbers = new [] {12.7, 52.1, 11.5};
             var grades = new List<double>() {12.7, 52.1, 11.5};
             grades.Add(100.01);
             
-            double test = 0;
+            var test = 0.0;
+            var highest = double.MinValue;
+            var lowest = double.MaxValue;
+            // var arr = new List<double>(){};
             foreach(double e in grades)
             {
                 test += e;
+                if(e > highest){
+                    highest = e;
+                }
+                lowest = Math.Min(e, lowest);
             }
+            System.Console.WriteLine($"The lowest grade is {lowest}");
+            System.Console.WriteLine($"The higher score is {highest}");
             test /= grades.Count;
-            System.Console.WriteLine($"The avarage grade is {test:N1}");
-
-            // System.Console.WriteLine(result);
-
-            
-
-
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else 
-            {
-                Console.WriteLine("Hello");
-            }
+            // Console.WriteLine(grades);
+            // System.Console.WriteLine(arr);
+            Console.WriteLine($"The avarage grade is {test:N1}");
         }
     };
+
+    
 }
