7d0b8ef99f357feb4d61c88f31eb0d8173b07bda
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-06 10:15:03 -0400

    inserting method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -9,11 +10,33 @@ namespace GradeBook
             grades = new List<double>();
             this.name = name;
         }
+        public void ShowStatistics()
+        {
+              var test = 0.0;
+            var highest = double.MinValue;
+            var lowest = double.MaxValue;
+            // var arr = new List<double>(){};
+            foreach(double e in grades)
+            {
+                test += e;
+                if(e > highest){
+                    highest = e;
+                }
+                lowest = Math.Min(e, lowest);
+            }
+            System.Console.WriteLine($"The lowest grade  is {lowest}");
+            System.Console.WriteLine($"The higher score is {highest}");
+            test /= grades.Count;
+            Console.WriteLine($"The avarage grade is {test:N1}");
+        }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
         private List<double> grades;
+
+        
+
         private string name;
         
     }
