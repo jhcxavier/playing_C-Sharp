68f80944a1c3badcf50be16a447ca6ac7a08b3c2
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-06 16:13:43 -0400

    pass by ref

--- test/GradeBook.Tests/TypeTest.cs
+++ test/GradeBook.Tests/TypeTest.cs
@@ -5,7 +5,20 @@ namespace GradeBook.Tests
 {
     public class TypeTest
     {
-         [Fact]
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+            
+        }
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+        [Fact]
         public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
