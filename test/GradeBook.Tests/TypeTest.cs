2637252650aa83917b5efc55e8471fc5bc54dccc
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-06 15:34:15 -0400

    testing value type

--- test/GradeBook.Tests/TypeTest.cs
+++ test/GradeBook.Tests/TypeTest.cs
@@ -13,6 +13,16 @@ namespace GradeBook.Tests
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);   
+            Assert.True(Object.ReferenceEquals(book1, book2));  
         }
 
         Book GetBook(string name)
