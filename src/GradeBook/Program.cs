492434fcb9e55cd371a71a378d4b7f03c9a7c3b6
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-07 15:16:47 -0400

    challenge done!

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,9 +9,34 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Joao's Book");
-            book.AddGrade(98.9);
-            book.AddGrade(90.5);
-            book.AddGrade(100.0);
+            // book.AddGrade(98.9);
+            // book.AddGrade(90.5);
+            // book.AddGrade(100.0);
+            
+            var rounds = 1;
+            for(var i = 0; i<rounds; i++)
+            {
+                System.Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if(input == "q"){
+                    break;
+                }else{
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                    rounds++;
+                }
+            }
+            // while(!done)
+            // {
+            //     System.Console.WriteLine("Enter a grade or 'q' to quit");
+            //     var input = Console.ReadLine();
+            //     var grade = double.Parse(input);
+            //     book.AddGrade(grade);
+            // }
+
+
+
+
             var stats = book.GetStatistics();
 
             System.Console.WriteLine($"The lowest grade  is {stats.Low}");
