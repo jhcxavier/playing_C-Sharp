9f1b0d68ae8bcded4c86edd95c82b91a715d3712
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-07 14:23:07 -0400

    switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -14,6 +14,21 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
         public void AddGrade(double grade)
         {
             if(grade <= 100 && grade >= 0)
